ba9824ee09359af6d290a60d0a783d9e9bbc10d1
BBite <bearbite190@gmail.com>
2021-01-30 21:14:11 +0200

    add shutdown notify

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from loader import dp
 # import filters
 import middlewares, handlers
 
-from utils.notify_admins import on_startup_notify
+from utils.notify_admins import on_startup_notify, on_shutdown_notify
 
 
 async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
+async def on_shutdown(disaptcher):
+    await on_shutdown_notify(disaptcher)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)

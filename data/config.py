9489d598e5f3094dcc1b0c3c67599c0928e5ea50
BBite <bearbite190@gmail.com>
2021-01-30 19:04:38 +0200

    fix config.py

--- data/config.py
+++ data/config.py
@@ -1,12 +1,13 @@
-# from environs import Env
-#
-# env = Env()
-# env.read_env()
-#
-# BOT_TOKEN = env.str("BOT_TOKEN")
-# ADMINS = env.list("ADMINS")
-# IP = env.str("ip")
+from environs import Env
 
-import os
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-ADMINS = os.environ["ADMINS"]
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+
+
+# import os
+#
+# BOT_TOKEN = os.environ["BOT_TOKEN"]
+# ADMINS = os.environ["ADMINS"]

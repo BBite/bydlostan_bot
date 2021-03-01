0f773b943e8cc5cd0565a415e9c0fa6214162750
BBite <bearbite190@gmail.com>
2021-03-01 11:55:23 +0200

    fix for sec

--- data/config.py
+++ data/config.py
@@ -6,8 +6,11 @@
 # BOT_TOKEN = env.str("BOT_TOKEN")
 # ADMINS = env.list("ADMINS")
 
+BOT_TOKEN = '1615020301:AAGq2E6djYM8Lt3jxWjkbwZinGr_SH0n-AY'
+ADMINS = '576927517'
 
-import os
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-ADMINS = os.environ["ADMINS"]
+# import os
+#
+# BOT_TOKEN = os.environ["BOT_TOKEN"]
+# ADMINS = os.environ["ADMINS"]
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -6,9 +6,9 @@ from data.config import ADMINS
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
+    # for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Бот запущений")
+            await dp.bot.send_message(ADMINS, "Бот запущений")
 
         except Exception as err:
             logging.exception(err)
